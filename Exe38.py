ee45e87f3cc2e531a32a8d81ccf306b7c3790005
prathishaprathisha <41039872+prathishaprathisha@users.noreply.github.com>
2018-12-20 15:18:38 +0530

    Add files via upload

---
+++ Exe38.py
@@ -0,0 +1,12 @@
+ 
+m= float(input(" Please Enter the First Value a: "))
+n = float(input(" Please Enter the Second Value b: "))
+
+print("Before Swapping two Number: m= {0} and n = {1}".format(m, n))
+
+temp = m
+m = n
+n = temp
+
+print("After Swapping two Number: a= {0} and n = {1}".format(m, n))
+
