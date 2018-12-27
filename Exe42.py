a53849f9f2299b2123ae177e0f653e38620ce64e
prathishaprathisha <41039872+prathishaprathisha@users.noreply.github.com>
2018-12-27 13:27:02 +0530

    Add files via upload

---
+++ Exe42.py
@@ -0,0 +1,12 @@
+print("Enter 'y' for exit.")
+str1 = input("Enter first string: ")
+if str1 == 'y':
+    exit();
+else:
+    str2 = input("Enter second string: ")
+    if str1 == str2:
+        print("\nBoth strings are equal to each other.")
+        print(string1,"==",string2);
+    else:
+        print("\nStrings are not equal.")
+        print(string1,"!=",string2);
